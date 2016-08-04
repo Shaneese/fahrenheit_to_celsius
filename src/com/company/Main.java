9822b31ca4dcc1cb993ded850ad6aebcb330ace0
Shaneese <shaneesesfdc@gmail.com>
2016-08-04 12:22:12 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double fahrenheit = 68.0;
+        double celsius;
+        celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println(celsius);
+    }
+}
